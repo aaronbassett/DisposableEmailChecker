9c5eb9aa4d8de3d3060c7c6551b1e726d7577f57
Aaron Bassett <me@aaronbassett.com>
2012-09-03 13:36:47 +0100

    Remove long description as causing pip error

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
-    readme = f.read()
 
 setup(
     name = "django-disposable-email-checker",
     version = "0.1.1",
     packages = find_packages(),
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-    },
     author = "Aaron Bassett",
     author_email = "me@aaronbassett.com",
     description = "Python class for use with Django to detect Disposable Emails",
-    long_description=readme,
     license = "MIT License",
     keywords = "django email disposable validation",
     url = "https://github.com/aaronbassett/DisposableEmailChecker",
