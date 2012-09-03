04cb5a1536460d532d3a7f100bbc54ff6f0c6a5e
Aaron Bassett <me@aaronbassett.com>
2012-09-03 12:27:09 +0100

    Add additional meta-data to package

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    readme = f.read()
 
 setup(
     name = "django-disposable-email-checker",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
     package_data = {
         # If any package contains *.txt or *.rst files, include them:
@@ -12,7 +14,21 @@ setup(
     author = "Aaron Bassett",
     author_email = "me@aaronbassett.com",
     description = "Python class for use with Django to detect Disposable Emails",
-    license = "MIT",
+    long_description=readme,
+    license = "MIT License",
     keywords = "django email disposable validation",
     url = "https://github.com/aaronbassett/DisposableEmailChecker",
+    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Framework :: Django'
+    ]
 )
