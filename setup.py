f960201acc5fcb8ce992b714aaadb37d2ef14a84
Aaron Bassett <me@aaronbassett.com>
2013-05-18 17:06:43 +0100

    Remove some legit providers who had been included

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-disposable-email-checker",
-    version = "0.2.2",
+    version = "0.2.3",
     packages = find_packages(),
     author = "Aaron Bassett",
     author_email = "me@aaronbassett.com",
