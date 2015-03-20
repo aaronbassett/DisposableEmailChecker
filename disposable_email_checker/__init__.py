327991c79736b8d46378131014141f6d6fd498d1
artscoop <artscoop93.info@gmail.com>
2015-03-20 16:57:07 +0100

    Make the checker Singleton
    No need to open the file anytime the checker is instanciated.
    One checker, one emails attribute is enough.
    If the file content changes, you can instantiate with a `refresh=True` argument.

--- disposable_email_checker/__init__.py
+++ disposable_email_checker/__init__.py
@@ -7,16 +7,29 @@ class DisposableEmailChecker():
     Check if an email is from a disposable
     email service
     """
-    
-    def __init__(self):
-        self.emails = [line.strip() for line in open(settings.DISPOSABLE_EMAIL_DOMAINS)]
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(DisposableEmailChecker, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+ 
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor
+        Use instances of the class like previously but make them
+        share only one class-level emails attribute.
+        @param refresh: equivalent to True for updating from file (e.g. file changed)
+        """
+        if not hasattr(DisposableEmailChecker, 'emails') or kwargs.get('refresh', False):
+            DisposableEmailChecker.emails = [line.strip() for line in open(settings.DISPOSABLE_EMAIL_DOMAINS)]
     
     def chunk(self, l, n):
         return (l[i:i + n] for i in range(0, len(l), n))
 
     def is_disposable(self, email):
-        for email_group in self.chunk(self.emails, 20):
-            regex = "(.*" + ")|(.*".join(email_group) + ")"
+        for email_group in self.chunk(DisposableEmailChecker.emails, 32):
+            regex = "(.*{0})".format(")|(.*".join(email_group))
             if re.match(regex, email):
                 return True
 
