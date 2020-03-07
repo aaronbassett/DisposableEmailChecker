703a3e2901e2bd00bb6be7cb32a3330453e47dd5
Jason Held <jasonsheld@gmail.com>
2020-03-07 13:00:55 -0500

    2.0.a2 bugfix on trove 3 only

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3:: Only",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
