3d2f150d7f4b343cb008f621429e2e395541b3f1
Jason Held <jasonsheld@gmail.com>
2020-07-24 13:25:40 -0400

    switch to gettext

--- disposable_email_checker/validators.py
+++ disposable_email_checker/validators.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.utils.encoding import force_text
 from django.core import validators
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from bdea.client import BDEAClient
 
